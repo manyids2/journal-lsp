6bf846c828ca299183243e05146eebdba1979ab4
manyids2 <arun.mukundan@gmail.com>
2022-07-31 18:29:52 +0200

    Can browse daily with preview, insert link seems ok, goto link is fucked

--- lua/journal-lsp/actions.lua
+++ lua/journal-lsp/actions.lua
@@ -2,24 +2,21 @@ require("journal-lsp.globals")
 
 local actions = {}
 
-function actions.find_files()
+function actions.find_files(root_dir, title)
 	local opts_with_preview, opts_without_preview
 
 	opts_with_preview = {
-		prompt_title = "~ all files ~",
-		shorten_path = false,
-
-		layout_strategy = "flex",
+		cwd = root_dir,
+		prompt_title = title,
+		shorten_path = true,
+		layout_strategy = "vertical",
 		layout_config = {
 			width = 0.9,
-			height = 0.8,
-
-			horizontal = {
-				width = { padding = 0.15 },
-			},
-			vertical = {
-				preview_height = 0.75,
-			},
+			height = 0.9,
+			preview_height = 0.7,
+			mirror = true,
+			anchor = "top",
+			prompt_position = "top",
 		},
 	}
 
@@ -30,21 +27,38 @@ function actions.find_files()
 end
 
 function actions.insert_link()
-	-- get context
 	local a = vim.api
 	local win = a.nvim_get_current_win()
 	local cursor = a.nvim_win_get_cursor(win)
 	vim.ui.input({ prompt = "->" }, function(input)
-    if (input == nil) then
-      return
-    end
-		a.nvim_buf_set_text(0,
-                        cursor[1] - 1,
-                        cursor[2] + 1,
-                        cursor[1] - 1,
-                        cursor[2] + 1,
-                        { "[" .. input .. "](notes/" .. input .. ".md)" })
+		if input == nil then
+			return
+		end
+		a.nvim_buf_set_text(
+			0,
+			cursor[1] - 1,
+			cursor[2] + 1,
+			cursor[1] - 1,
+			cursor[2] + 1,
+			{ "[" .. input .. "](notes/" .. input .. ".md)" }
+		)
 	end)
+end
+
+function actions.goto_link()
+	local ts_utils = require("nvim-treesitter.ts_utils")
+	local node = ts_utils.get_node_at_cursor()
+	local link = node.parent(node)
+	if link.type(node) == "inline_link" then
+		local text = vim.treesitter.query.get_node_text(link.child(link), 0)
+    local name = vim.split(text, ']')[1]
+    local path = vim.split(text, ']')[2]
+    name = name:sub(2, -1)
+    path = path:sub(2, -2)
+		if path then
+      vim.api.nvim_command('e ' .. path)
+		end
+	end
 end
 
 return actions
--- lua/journal-lsp/init.lua
+++ lua/journal-lsp/init.lua
@@ -1,32 +1,64 @@
 local M = {}
 
 M.name = "journal-lsp"
-M.filetypes = {"markdown", "telekasten"}
+M.filetypes = { "markdown", "telekasten" }
 M.enabled = false
 
-M.setup = function ()
-  require("journal-lsp.globals")
+M.setup = function()
+	require("journal-lsp.globals")
 
-  -- 1. is it md file?
-  if not CE(M.filetypes, vim.bo.filetype) then
-    return
+	-- 1. is it md file?
+	if not CE(M.filetypes, vim.bo.filetype) then
+		return
+	end
+
+	-- 2. is it 'journal' dir?
+	local root_dir = (vim.fs.dirname(vim.fs.find({
+		"index.md",
+		"README.md",
+	}, { upward = true })[1]))
+	if root_dir then
+		M.root_dir = root_dir
+		M.enabled = true
+	else
+		require("index.md not found.")
+		return
+	end
+
+	-- 3. is it 'daily' or 'weekly' note?
+	local s = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+	local date = "%d+-%d+-%d+.md"
+	if not (string.find(s, date) == nil) then
+		-- get the date
+		local _, _, y, m, d = string.find(s, "(%d+)-(%d+)-(%d+).md")
+		print(y, m, d)
+		print(M.root_dir)
+
+    -- TODO: get yesterday and tomorrow
+    M._yesterday = y .. '-' .. m .. '-' .. y .. '.md'
+    M._tomorrow = y .. '-' .. m .. '-' .. y .. '.md'
+	end
+
+	-- 4. init
+	vim.notify("journal-lsp started")
+	local actions = require("journal-lsp.actions")
+
+  M.files = function ()
+    actions.find_files(M.root_dir, "~ files ~")
   end
 
-  -- 2. is it 'journal' dir?
-  local root_dir = (vim.fs.dirname(vim.fs.find({
-      'index.md', 'README.md'
-    }, {upward = true})[1]))
-  if (root_dir) then
-    M.root_dir = root_dir
-    M.enabled = true
-  else
-    require("index.md not found.")
-    return
+  M.daily = function ()
+    actions.find_files(M.root_dir .. '/daily', "~ daily ~")
   end
 
-  -- 3. init
-  vim.notify('journal-lsp started')
-  M.actions = require("journal-lsp.actions")
+  M.weekly = function ()
+    actions.find_files(M.root_dir .. '/weekly', "~ weekly ~")
+  end
+
+  M.goto_link = function ()
+    actions.goto_link()
+  end
+
 end
 
 return M
